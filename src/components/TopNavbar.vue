e92a72a5ec26c5b0c30dd97de01a28b342cd95b5
4upak <serg.chupak@gmail.com>
2022-11-04 16:38:16 +0200

    navbar component

---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid>
+    <v-app-bar
+        elevation="4"
+        color="#fff"
+    >
+      <v-tabs
+          align-with-title
+          color="#6e6d73"
+      >
+        <v-tab>Home</v-tab>
+        <v-tab>Exchanges</v-tab>
+        <v-tab>Articles</v-tab>
+        <v-tab>F.A.Q</v-tab>
+        <v-tab>Help</v-tab>
+        <v-tab>Contacts</v-tab>
+      </v-tabs>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
